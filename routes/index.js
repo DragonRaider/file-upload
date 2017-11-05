a4aeec9c74e7c69d0af79a6c9b712ed07bcf08b0
DragonRaider996 <anant.130410116086@gmail.com>
2017-11-05 23:59:27 +0530

    Changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('./config.json')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/aws',(req,res)=>{
+  const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+  s3.listBuckets((err,data)=>{
+    if(err){
+      console.log(err);
+    }else{
+      console.log(data.Buckets);
+      res.send("success");
+    }
+  });
+
+});
+
+
 
 module.exports = router;
--- routes/upload.js
+++ routes/upload.js
@@ -1,31 +1,45 @@
 const multer = require("multer");
 const _ = require("lodash");
+const fs = require("fs");
+
 var express = require('express');
 var router = express.Router();
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('./config.json')
 
-var storage = multer.diskStorage({
-    destination:(req,file,callback)=>{
-      callback(null,"./public/images");
-    },
-    filename:(req,file,callback)=>{
-      var temp = file.originalname.split(".");
-      var name = _.dropRight(temp).join(".");
-      var extension = _.last(temp);
-      console.log(name);
-      callback(null,name+"-"+Date.now()+"."+extension);
-    }
-});
+var upload = multer({ dest: './public/images' });
 
-var upload = multer({storage:storage}).array("images");
-
-router.post("/",(req,res)=>{
-    upload(req,res,(err)=>{
-        if(err){
-            console.log(err);
-            res.render("error",{message:"Sorry Could Not Upload the Images"});
-        }
-        res.render("success");
+router.post("/",upload.single("images"),(req,res)=>{
+    const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+    let uploadParams = {Bucket: 'pocketdoc', Key: '', Body: ''};
+    var filepath = req.file.path;
+    console.log(filepath);
+    var fileStream = fs.createReadStream(filepath);
+    fileStream.on('error',(err)=>{
+        console.log(err);
     });
+    var temp = req.file.originalname.split(".");
+    var name = _.dropRight(temp).join(".");
+    var extension = _.last(temp);
+    var name = name+"-"+Date.now()+"."+extension;
+
+    var temp1 = filepath.split("/");
+    var path = _.dropRight(temp1).join("/");
+    uploadParams.Body = fileStream;
+    uploadParams.Key = path+"/"+name;
+    
+    s3.upload(uploadParams,(err,data)=>{
+        if(err){
+            res.render("error",{message:"Sorry Could Not Upload the Images"});
+        }if(data){
+            res.render("success");
+            fs.unlink(filepath,(err)=>{
+                if(err){
+                    console.log("Couldn't delete the File");
+                }
+            });
+        }
+    })
 })
 
 module.exports = router;
